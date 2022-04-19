cb724ce711b3857e5a7c81d23d75cb89695c9eee
jord2097 <100094374+jord2097@users.noreply.github.com>
2022-04-19 15:33:39 +0100

    backend ready for routing

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const port = process.env.PORT || 3000
+const mongoose = require('mongoose') 
+const { v4: uuidv4 } = require('uuid') // tokens
+const uri = "mongodb+srv://jord2097:97f514dVaZWLrF0J8gqs@cluster0.thipn.mongodb.net/blog-data?retryWrites=true&w=majority" // MongoDB uri
+
+mongoose.connect(uri)
+
+const app = express()
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+
+app.listen(port, () => {
+    console.log(`Blog API listening on http://localhost:${port}`)
+})
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, "Connection error"))
+db.once('open', function callback() {
+    console.log("Database connected")
+})
+
