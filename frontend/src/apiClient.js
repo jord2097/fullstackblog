37f0db78fc690efe17a5b9c3f97cb2f500c6cc12
jord2097 <100094374+jord2097@users.noreply.github.com>
2022-04-21 10:58:30 +0100

    added apiclient for frontend

---
+++ frontend/src/apiClient.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+const url = 'http://localhost:3000' // URL for the API server
+
+export class apiClient {
+    constructor(token) {
+        this.token = token
+    }
+
+    apiCall(method,url,data) {
+        return axios({
+            method,
+            url,
+            data,
+        }).catch((error) => {
+            throw error
+        })
+    }
+
+    // basic operations
+
+    getPosts() {
+        return this.apiCall("get", `${url}/posts`)
+    }
+
+    createPost(title, mainText, img, category, tags, draft, published) {
+        return this.apiCall("post", `${url}/create`, { title, mainText, img, category, tags, draft, published })
+    }
+
+    updatePost(_id, title, mainText, img, category, tags, draft, published) {
+        return this.apiCall("put", `${url}/${_id}`, { title, mainText, img, category, tags, draft, published })
+    }
+
+    deletePost(_id) {
+        return this.apiCall("delete", `${url}/${_id}`)
+    }
+
+    // extra features
+
+    searchCategory(category) {
+        return this.apiCall("get", `${url}/search/category`, {category})        
+    }
+
+    searchTags(tags) {
+        return this.apiCall("get", `${url}/search/tags`, {tags} )
+    }
+
+    showDrafts() {
+        return this.apiCall("get", `${url}/posts/drafts`)
+    }
+
+}
