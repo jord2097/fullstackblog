da444aa4c677fc1f6d9ce253706b6e0b27749ba4
jord2097 <100094374+jord2097@users.noreply.github.com>
2022-04-25 09:38:20 +0100

    mvp as presented

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
 import './sidebar.css';
+import Add from '../CreateNewPost.js'
 
-export default function Sidebar() {
+export default function Sidebar(props) {
   return (
     <div className='sidebar'>
-        <div className="sidebarItem">
+        {/* <div className="sidebarItem">
             <span className='sidebarTitle'>about me</span>
             <img 
             src='https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2016/03/fall-trees-road-1.jpg' alt='haha'
@@ -11,7 +12,9 @@ export default function Sidebar() {
             <p>
             How to create a blog website using React.js. Blog app React project from scratch for beginners. Design React blog app using functional React components and React Router Dom.
             </p>
-        </div>
-        </div>
+        </div> */}
+        <Add client={props.client} refreshList={props.refreshList} current={props.current} cCurrent={props.cCurrent}/>
+    </div>
+    
   )
 }
