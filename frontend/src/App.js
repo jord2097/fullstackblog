e1bc76c761b28d70007cf7429562f38843b92a1d
haf-abe <haftuabebe52@gmail.com>
2022-04-26 12:23:32 +0100

    register backup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 function App() {
   return (
     <>
-      <TopBar/>
-      <Home/>
+     <Register/>
+      
+     
     </>
   );
 }
--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -7,8 +7,70 @@ export default function Post() {
     <div className='post'>
         <img
         className='postImg'
-        src='https://iso.500px.com/wp-content/uploads/2014/08/500-px-banner-1500x1000.jpg'
+        src='https://wallsdesk.com/wp-content/uploads/2017/01/Newfoundland-HD-Wallpaper.jpg'
         alt=''
+        />
+
+        <div className="postInfo"></div>
+            <div className="postCats">
+                <span className="postCat">Travel</span>
+                <span className="postCat">go wild</span>   
+            </div>
+            <span className="postTitle"> Great journey </span>
+            <hr/>
+            {/* hr adds line */}
+            <span className="postDate"> 2 day ago</span>
+     
+     <div className='postDescContainer'>
+         <p className='postDesc'>
+         What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+         </p>
+    </div>
+     </div>
+
+
+     <div className='post'>
+        <img
+        className='postImg'
+        src='http://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images.jpg'
+        alt='beach'
+        />
+
+        <div className="postInfo"></div>
+            <div className="postCats">
+                <span className="postCat">Nature</span>
+                <span className="postCat">Nature is great</span>   
+            </div>
+            <span className="postTitle"> a great time at </span>
+            <hr/>
+            {/* hr adds line */}
+            <span className="postDate"> 1 day ago</span>
+     
+     <div className='postDescContainer'>
+         <p className='postDesc'>
+         What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+         </p>
+    </div>
+     </div>
+     
+
+     <div className='post'>
+        <img
+        className='postImg'
+        src='http://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images.jpg'
+        alt='beach'
         />
 
         <div className="postInfo"></div>
@@ -33,7 +95,6 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
          </p>
     </div>
      </div>
-     
 
      </>
 
